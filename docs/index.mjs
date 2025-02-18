d5318f2d0e40e493e43dcdf8960617bfc817075f
ChineseMuffin <61895374+ChineseMuffin@users.noreply.github.com>
2025-02-18 21:28:32 +0900

    モダンなデザインに変更

--- docs/index.mjs
+++ docs/index.mjs
@@ -2,15 +2,15 @@
 import * as transfer from "./transfer.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const balance1 = document.getElementById("balance1");
-    const balance2 = document.getElementById("balance2");
+    const balanceFrom = document.getElementById("balanceFrom");
+    const balanceTo = document.getElementById("balanceTo");
     function onChange(event) {
-        const goodTransferH = document.getElementById("goodTransfer");
-        const account1 = new transfer.Account(0, 1000000, parseInt(balance1.value));
-        const account2 = new transfer.Account(0, 20000, parseInt(balance2.value));
+        const result = document.getElementById("result");
+        const account1 = new transfer.Account(0, 1000000, parseInt(balanceFrom.value));
+        const account2 = new transfer.Account(0, 20000, parseInt(balanceTo.value));
         const goodTransfer = new transfer.Transfer(account1, account2, transfer.Transfer.TARGET.FROM, 1000);
-        goodTransferH.textContent = goodTransfer.calculate();
+        result.textContent = goodTransfer.calculate();
     }
-    balance1.addEventListener("change", onChange);
-    balance2.addEventListener("change", onChange);
+    balanceFrom.addEventListener("change", onChange);
+    balanceTo.addEventListener("change", onChange);
 });
